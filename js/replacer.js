68a54640e886cdcca058e3687eed08e0ef3123f4
MustardBell <mustardbellscifi@gmail.com>
2024-03-05 17:01:08 +0200

    Implemented custom Markdown syntax
    Such as:
    
    ~#small underlined text#~
    
    conversion of indenteed text into [indent][/indent] rather than [code][/code]
    
    arrows conversions

--- js/replacer.js
+++ js/replacer.js
@@ -1,3 +1,4 @@
+
 function mdToBBCode(text) {
     return text
         //
@@ -5,6 +6,8 @@ function mdToBBCode(text) {
         //
         .replace(/^(.+)(\n|\r\n)([=]{3,3}\s*(\n|\r\n)?)$/gmi, "[h1]$1[/h1]") // h1
         .replace(/^(.+)(\n|\r\n)([-]{3,3}\s*(\n|\r\n)?)$/gmi, "[h2]$1[/h2]") // h2
+        //
+        .replace(/~/gmi, '[u][size=1]$1[/size][/u]') // Custom Markdown syntax. Signature
         //
         .replace(/^[#]{6,6}\s*(.*[^\s])\s*[#]{6,6}\n?$/gmi, "[h6]$1[/h6]") // h4
         .replace(/^[#]{6,6}\s*(.*)\n?$/gmi, "[h6]$1[/h6]") // h4
@@ -20,20 +23,34 @@ function mdToBBCode(text) {
         .replace(/^[#]{3,3}\s*(.*[^\s])\s*[#]{3,3}\n?$/gmi, "[h3]$1[/h3]") // h3
         .replace(/^[#]{3,3}\s?(.*)\n?$/gmi, "[h3]$1[/h3]") // h3
         //
-        .replace(/^\`{3,3}(.*)\n((?:.|\n)+?)\n\`{3,3}\n?$/gmi, "[code]$2[/code]") // back ticks
-        .replace(/^\~{3,3}(.*)\n((?:.|\n)+?)\n\~{3,3}\n?$/gmi, "[code]$2[/code]") // tilde
-        .replace(/\`([^\`].*?)\`/gmi, "[code single]$1[/code]") // inline code
-
+        .replace(/^[ ]{20,20}([^\s].*)\n?$/gmi, "[indent=5]$1[/indent]") // instead of making indents code, make them indents
+        .replace(/^[ ]{16,16}([^\s].*)\n?$/gmi, "[indent=4]$1[/indent]") // instead of making indents code, make them indents
+        .replace(/^[ ]{12,12}([^\s].*)\n?$/gmi, "[indent=3]$1[/indent]") // instead of making indents code, make them indents
+        .replace(/^[ ]{8,8}([^\s].*)\n?$/gmi, "[indent=2]$1[/indent]") // instead of making indents code, make them indents
+        .replace(/^[ ]{4,4}([^\s].*)\n?$/gmi, "[indent]$1[/indent]") // instead of making indents code, make them indents
+        //     ARROWS
+        .replace(/=\>/gmi, "►")
+        .replace(/\<=/gmi, "◄")
+        .replace(/\<-\>/gmi, "↔")
+        .replace(/\*\*\s*-\>\s*\*\*/gmi, "[size=6]→[/size]")
+        .replace(/\*\*\s*\<-\s*\*\*/gmi, "[size=6]←[/size]")
+        .replace(/-\>/gmi, "→")
+        .replace(/\<-/gmi, "←")
+        //
+        .replace(/^\`{3,3}(.*)\n((?:.|\n)+?)\n\`{3,3}\n?$/gmi, "[code]$2[/code]")   // back ticks
+        .replace(/^\~{3,3}(.*)\n((?:.|\n)+?)\n\~{3,3}\n?$/gmi, "[code]$2[/code]")   // tilde
+        .replace(/\`([^\`].*?)\`/gmi, "[code single]$1[/code]")  // inline code
         //
         .replace(/\[!\[.*?\]\((.*?)\)\]\((.*?)\)/gmi, '[img]$1[/img]\nClickable image was pointing to [url=$2]here[/url]') //image
         .replace(/!\[(.*)\]\((.*)\)/gmi, '[img]$2[/img]') //image
-
         //
-        .replace(/\*\*\*([^\*].*?)\*\*\*/gmi, "[b][i]$1[/i][/b]") // bold + itlaic
-        .replace(/\*\*([^\*].*?)\*\*/gmi, "[b]$1[/b]") // bold
-        .replace(/(\s)\*((?!\/[0-9]|\s|\*).*?)\*(\s)/gmi, "$1[i]$2[/i]$3");
+        .replace(/(?<!#)\*\*([^\*].*?\*?)\*\*/gmi, "[b]$1[/b]") // bold
+        .replace(/(^|\s|indent(?:=\d)?\]|\[|<| )\*(?!\*+\s)(.*?[^\*].*?)\*(?<!\s\*)/gmi, "$1[i]$2[/i]")
+        //  MDASH
+        .replace(/(?<=[\w,!?'" ])--(?:[ \w,!?'"])/gmi, "—") 
+        .replace(/ - /gmi, "—") 
+    //.replace(/^>(.+)$/,"[quote]$1[/quote]")
 }
-
 function convert()
     {
     var left = document.getElementById("left_ta");
