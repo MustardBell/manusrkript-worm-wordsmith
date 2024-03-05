a9c3c1408cabd6a9b93e87cf72ed0f1ec359053b
MustardBell <mustardbellscifi@gmail.com>
2024-03-05 16:51:02 +0200

    Extracted converter to a separate function

--- js/replacer.js
+++ js/replacer.js
@@ -1,13 +1,7 @@
-function convert()
-    {
-    var left = document.getElementById("left_ta");
-    var right = document.getElementById("right_ta");
-    
-    var left_value = left.value;
-        //general Markdown conversion
-        left_value = left_value
+function mdToBBCode(left_value) {
+    return left_value
         //
-        .replace(/\[(.+?)\]\((.+?)(\s".*")?\)/gmi,'[url=$2]$1[/url]') // url
+        .replace(/\[(.+?)\]\((.+?)(\s".*")?\)/gmi, '[url=$2]$1[/url]') // url
         //
         .replace(/^(.+)(\n|\r\n)([=]{3,3}\s*(\n|\r\n)?)$/gmi, "[h1]$1[/h1]") // h1
         .replace(/^(.+)(\n|\r\n)([-]{3,3}\s*(\n|\r\n)?)$/gmi, "[h2]$1[/h2]") // h2
@@ -33,18 +27,32 @@ function convert()
         //.replace(/^(?!\n*[`]{3,3})[#]{1,1}\s*(.*)\n?$/gmi, "[h1]$1[/h1]") // h1
         //
         //.replace(/^\t|[ ]{4,4}(.*)/gmi, "$1")  //code; strips tabs
-        .replace(/^\`{3,3}(.*)\n((?:.|\n)+?)\n\`{3,3}\n?$/gmi, "[code]$2[/code]")   // back ticks
-        .replace(/^\~{3,3}(.*)\n((?:.|\n)+?)\n\~{3,3}\n?$/gmi, "[code]$2[/code]")   // tilde
-        .replace(/\`([^\`].*?)\`/gmi, "[code single]$1[/code]")  // inline code
+        .replace(/^\`{3,3}(.*)\n((?:.|\n)+?)\n\`{3,3}\n?$/gmi, "[code]$2[/code]") // back ticks
+        .replace(/^\~{3,3}(.*)\n((?:.|\n)+?)\n\~{3,3}\n?$/gmi, "[code]$2[/code]") // tilde
+        .replace(/\`([^\`].*?)\`/gmi, "[code single]$1[/code]") // inline code
+
         //
-        .replace(/\[!\[.*?\]\((.*?)\)\]\((.*?)\)/gmi,'[img]$1[/img]\nClickable image was pointing to [url=$2]here[/url]') //image
-        .replace(/!\[(.*)\]\((.*)\)/gmi,'[img]$2[/img]') //image
+        .replace(/\[!\[.*?\]\((.*?)\)\]\((.*?)\)/gmi, '[img]$1[/img]\nClickable image was pointing to [url=$2]here[/url]') //image
+        .replace(/!\[(.*)\]\((.*)\)/gmi, '[img]$2[/img]') //image
+
         //
         .replace(/\*\*\*([^\*].*?)\*\*\*/gmi, "[b][i]$1[/i][/b]") // bold + itlaic
         .replace(/\*\*([^\*].*?)\*\*/gmi, "[b]$1[/b]") // bold
-        .replace(/(\s)\*((?!\/[0-9]|\s|\*).*?)\*(\s)/gmi, "$1[i]$2[/i]$3")  // italic
+        .replace(/(\s)\*((?!\/[0-9]|\s|\*).*?)\*(\s)/gmi, "$1[i]$2[/i]$3");
+}
+
+function convert()
+    {
+    var left = document.getElementById("left_ta");
+    var right = document.getElementById("right_ta");
+    
+    var left_value = left.value;
+        //general Markdown conversion
+        left_value = mdToBBCode(left_value)  // italic
         //
         right.value = left_value;
         console.log('converted');
+
+
     }
     
